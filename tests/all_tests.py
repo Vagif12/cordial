110e37c3b5d3c91339eb3e520232757aa73dba7d
Vagif <vagal2003@gmail.com>
2020-08-14 22:12:57 +0100

    adding unit tests

---
+++ tests/all_tests.py
@@ -0,0 +1,14 @@
+import unittest
+from cordial.content_recommenders import GraphRecommender, BasicRecommender
+
+class TestQueries(unittest.TestCase):
+    def test_graph_recommender(self):
+        recommender = GraphRecommender('disney',text_feature='plot')
+        assert 'result' in recommender.recommend('Coco')
+
+        def test_basic_recommender(self):
+        recommender = BasicRecommender('disney',text_feature='plot')
+        assert 'result' in recommender.recommend('Toy Story')
+
+if __name__ == '__main__':
+    unittest.main()
