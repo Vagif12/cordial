8f888f92b4b099360fe72927b091e4f008b2497a
Vagif <vagal2003@gmail.com>
2020-08-14 22:40:36 +0100

    final fix on travis ci

---
+++ cordial/tests/all_tests.py
@@ -0,0 +1,18 @@
+import unittest
+from cordial.content_recommenders import GraphRecommender, BasicRecommender
+
+class TestQueries(unittest.TestCase):
+    """
+    Test recommender system queries for Travis
+    """
+
+    def test_graph_recommender(self):
+        recommender = GraphRecommender('disney',text_feature='plot')
+        assert 'result' in recommender.recommend('Coco')
+
+    def test_basic_recommender(self):
+        recommender = BasicRecommender('disney')
+        assert 'result' in recommender.recommend('Toy Story')
+
+if __name__ == '__main__':
+    unittest.main()
